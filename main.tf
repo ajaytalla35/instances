24ab6d3f1d36702e3fcad455211037f4bc44a6dd
ajaytalla35 <ajay.talla35@gmail.com>
2025-03-04 13:02:32 +0530

    adding main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "my_vm" {
+ami = "ami-05b10e08d247fb927"
+instance_type = "t2.micro"
+subnet_id = data.terraform_remote_state.network_details.outputs.aws_subnet
+key_name = data.terraform_remote_state.network_details.outputs.aws_key_pair
+security_groups = [data.terraform_remote_state.network_details.outputs.aws_security_group]
+tags = {
+Name = "student.ajay-vm1"
+}
+}
+
